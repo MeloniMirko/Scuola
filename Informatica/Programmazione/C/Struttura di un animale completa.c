d29070eafe87c3cc41bd4ee01f3e6df68ad6a875
IlMaestro <melonimirko@asproni-fermi.edu.it>
2025-09-26 00:33:35 +0200

    Commit automatico 26/09/2025  0:33:35,82

--- Informatica/Programmazione/C/Struttura di un animale completa.c
+++ Informatica/Programmazione/C/Struttura di un animale completa.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct
-{
+
+typedef struct {
     char razza[20];
     char specie[20];
     char colore[20];
     int peso;
-    int altezza;
-};Animale;
+    float altezza;
+} Animale;
 
+void stampaAnimale(Animale Dado);
 
-int main(){
-    
-    Animale Dado; //dichiarazione della variabile
-    
-    printf("che razza è il tuo animale domestico?");
-    scanf("%s",Dado.razza);
-    printf("Di che specie e'?");
-    scanf("%s",Dado.specie);
-    printf("Di che colore e'?");
+int main() {
+    Animale Dado; //dichiarazione della variabile Dado di tipo
+//  tipo    nome    
+
+    printf("Che razza e' il tuo animale domestico? ");
+    scanf("%s", Dado.razza);
+    printf("Di che specie e' ? ");
+    scanf("%s", Dado.specie);
+    printf("Di che colore e' ? ");
     scanf("%s",Dado.colore);
-    printf("Quanto pesa?");
-    scanf("%d",Dado.peso);
-    printf("Quanto e' alto?");
-    scanf("%d0,Dado.peso");
+    printf("Quanto pesa? ");
+    scanf("%d", &Dado.peso);
+    printf("Quanto e' alto? ");
+    scanf("%f", &Dado.altezza);
+    stampaAnimale(Dado);
 
+}
+
+
+void stampaAnimale(Animale Dado) {
+    printf("\nIl tuo animale domestico e' un %s", Dado.razza);
+    printf("\nLa razza e' %s", Dado.specie);
+    printf("\nIl colore e' %s", Dado.colore);
+    printf("\nPesa %d Kg", Dado.peso);
+    printf("\nE' alto %.2f cm",Dado.altezza);
 }
