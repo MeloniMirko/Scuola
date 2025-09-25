c2333a18ac2f907ea37a301365bd63f7612e5b3f
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-09-25 09:22:22 +0200

    Auto-sync commit 25/09/2025  9:22:21,91

--- Informatica/Programmazione/C/Struttura di un animale completa.c
+++ Informatica/Programmazione/C/Struttura di un animale completa.c
@@ -3,5 +3,27 @@
 
 typedef struct
 {
+    char razza[20];
+    char specie[20];
+    char colore[20];
+    int peso;
+    int altezza;
+};Animale;
+
+
+int main(){
     
-};
+    Animale Dado; //dichiarazione della variabile
+    
+    printf("che razza è il tuo animale domestico?");
+    scanf("%s",Dado.razza);
+    printf("Di che specie e'?");
+    scanf("%s",Dado.specie);
+    printf("Di che colore e'?");
+    scanf("%s",Dado.colore);
+    printf("Quanto pesa?");
+    scanf("%d",Dado.peso);
+    printf("Quanto e' alto?");
+    scanf("%d0,Dado.peso");
+
+}
