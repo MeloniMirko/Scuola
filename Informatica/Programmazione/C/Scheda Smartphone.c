18bc2ef621f9c51897d71e362f56e963087d1e77
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-09-26 09:25:21 +0200

    Commit automatico 26/09/2025  9:25:21,03

--- Informatica/Programmazione/C/Scheda Smartphone.c
+++ Informatica/Programmazione/C/Scheda Smartphone.c
@@ -19,15 +19,19 @@ typedef struct{
     double peso;       // peso in grammi
 }Smartphone;
 
+void stampaSmartphone(Smartphone s)
+
+
+
 int main(){
     Smartphone s1, s2;
 
     // Input per il primo smartphone
     printf("Inserisci i dati del primo smartphone:\n");
     printf("Modello: ");
-    scanf("%39s", s1.modello);
+    scanf("%s[^\n]%*c", s1.modello);
     printf("Marca: ");
-    scanf("%29s", s1.marca);
+    scanf("%s", s1.marca);
     printf("Memoria (GB): ");
     scanf("%d", &s1.memoriaGB);
     printf("RAM (GB): ");
@@ -38,9 +42,9 @@ int main(){
     // Input per il secondo smartphone
     printf("\nInserisci i dati del secondo smartphone:\n");
     printf("Modello: ");
-    scanf("%39s", s2.modello);
+    scanf("%s", s2.modello);
     printf("Marca: ");
-    scanf("%29s", s2.marca);
+    scanf("%s", s2.marca);
     printf("Memoria (GB): ");
     scanf("%d", &s2.memoriaGB);
     printf("RAM (GB): ");
@@ -49,12 +53,15 @@ int main(){
     scanf("%lf", &s2.peso);
 
     // Stampa dei dati inseriti
-    printf("\nDati del primo smartphone:\n");
+  
+
+    void stampaSmartphone(Smartphone s1){
     printf("Modello: %s\n", s1.modello);
     printf("Marca: %s\n", s1.marca);
     printf("Memoria (GB): %d\n", s1.memoriaGB);
     printf("RAM (GB): %d\n", s1.ramGB);
     printf("Peso (grammi): %.2f\n", s1.peso);
+}
 
     printf("\nDati del secondo smartphone:\n");
     printf("Modello: %s\n", s2.modello);
