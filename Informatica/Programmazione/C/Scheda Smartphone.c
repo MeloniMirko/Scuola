746fd8ccf78b0086e5fef1f1a545eb496424dbde
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-09-26 09:35:27 +0200

    Commit automatico 26/09/2025  9:35:27,03

--- Informatica/Programmazione/C/Scheda Smartphone.c
+++ Informatica/Programmazione/C/Scheda Smartphone.c
@@ -29,7 +29,7 @@ int main(){
     // Input per il primo smartphone
     printf("Inserisci i dati del primo smartphone:\n");
     printf("Modello: ");
-    scanf("%[^\n]s", s.modello);
+    scanf("%[^\n]s", s1.modello);
     printf("Marca: ");
     scanf("%s", s1.marca);
     printf("Memoria (GB): ");
@@ -55,24 +55,24 @@ int main(){
     // Stampa dei dati inseriti
 
 
-    void stampaSmartphone(Smartphone s1){
-    printf("Modello: %s\n", s1.modello);
-    printf("Marca: %s\n", s1.marca);
-    printf("Memoria (GB): %d\n", s1.memoriaGB);
-    printf("RAM (GB): %d\n", s1.ramGB);
-    printf("Peso (grammi): %.2f\n", s1.peso);
-    printf("\nDati del secondo smartphone:\n");
-    printf("Modello: %s\n", s2.modello);
-    printf("Marca: %s\n", s2.marca);
-    printf("Memoria (GB): %d\n", s2.memoriaGB);
-    printf("RAM (GB): %d\n", s2.ramGB);
-    printf("Peso (grammi): %.2f\n", s2.peso);
+
+
+
+    printf ("risultati primo smartphone:\n");
+    stampaSmartphone(s1);
+    printf ("\nrisultati secondo smartphone:\n");
+    stampaSmartphone(s2);
+
+    return 0;
 }
-
-
-
-    
-
+    void stampaSmartphone(Smartphone s){
+        printf("Modello: %s\n", s.modello);
+        printf("Marca: %s\n", s.marca);
+        printf("Memoria (GB): %d\n", s.memoriaGB);
+        printf("RAM (GB): %d\n", s.ramGB);
+        printf("Peso (grammi): %.2f\n", s.peso);
+    }
+     
    
     
     
@@ -80,4 +80,3 @@ int main(){
 
 
 
-}
