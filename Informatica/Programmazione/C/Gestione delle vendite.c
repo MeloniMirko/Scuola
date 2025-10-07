e6cb6429d28347f8149fd0835bbbbc7bde72c548
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-10-07 12:49:47 +0200

    Commit automatico 07/10/2025 12:49:47,39

---
+++ Informatica/Programmazione/C/Gestione delle vendite.c
@@ -0,0 +1,18 @@
+/*Un'azienda desidera monitorare le vendite dei propri prodotti. Ogni vendita contiene le seguenti informazioni:
+Codice della vendita (un intero)
+Nome del prodotto (una stringa)
+Quantità venduta (un intero)
+Prezzo unitario del prodotto (un float)
+Data della vendita (espressa con una struttura in cui si memorizza giorno, mese, anno)
+Si desidera realizzare un programma in C che permetta di:
+- Definire una struttura per memorizzare le informazioni relative a una vendita.
+
+Specifiche:
+Utilizzare una struttura con typedef per definire il tipo di dato relativo a una vendita.
+Gestire la data come una sottostruttura all'interno della struttura vendita.
+Prevedere un array di vendite.
+FACOLTATIVO
+Implementare un array per memorizzare più vendite e permettere le seguenti operazioni:
+Aggiungere una nuova vendita all'array.
+Visualizzare le informazioni relative a tutte le vendite effettuate.
+Calcolare il totale delle vendite effettuate in un determinato anno.*/
