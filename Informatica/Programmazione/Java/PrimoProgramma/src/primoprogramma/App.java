3eac6695a286982bb60a399a29a2f00c60fcdf17
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-10-14 13:18:48 +0200

    Commit automatico 14/10/2025 13:18:48,57

--- Informatica/Programmazione/Java/PrimoProgramma/src/primoprogramma/App.java
+++ Informatica/Programmazione/Java/PrimoProgramma/src/primoprogramma/App.java
@@ -1,6 +1,22 @@
+//Realizzare un programma che dati due valori interi memorizzati nelle variabili varA e varB.
+// In seguito, scambi il valore di tali variabili e stampi a schermo il nuovo contenuto.
+
 package primoprogramma;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int varA = 5;
+        int varB = 10;
+        int temp;
+
+        System.out.println("Valore iniziale di varA: " + varA);
+        System.out.println("Valore iniziale di varB: " + varB);
+
+        // Scambio dei valori
+        temp = varA;
+        varA = varB;
+        varB = temp;
+
+        System.out.println("Nuovo valore di varA: " + varA);
+        System.out.println("Nuovo valore di varB: " + varB);
     }
 }
