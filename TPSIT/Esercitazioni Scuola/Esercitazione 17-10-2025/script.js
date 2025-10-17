dbc01be2fb6b93fb801200e633479bdb952a7150
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-10-17 12:32:04 +0200

    Commit automatico 17/10/2025 12:32:04,77

--- TPSIT/Esercitazioni Scuola/Esercitazione 17-10-2025/script.js
+++ TPSIT/Esercitazioni Scuola/Esercitazione 17-10-2025/script.js
@@ -1,12 +1,13 @@
-function verificaAccesso() {
+function verificaAccessoDefraiaManolo() {
     let password = prompt("Inserisci la password");
     let codice = prompt("Inserisci il codice segreto");
 
     if (password === "admin" && codice === "0000") {
-        alert("Accesso speciale");
+        alert("Accesso speciale come mirko");
     } else if (password === "admin") {
         alert("Codice errato");
     } else {
         alert("Accesso negato");
     }
+    alert("Mirko sei speciale come Manolo");
 }
