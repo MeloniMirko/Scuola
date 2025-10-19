5dfc53d14181a820dec58cf59f955062c7303eee
IlMaestro <melonimirko@asproni-fermi.edu.it>
2025-10-19 19:10:18 +0200

    Commit automatico 19/10/2025 19:10:18,46

--- Informatica/Programmazione/Java/Anagrafica/src/elenco/Main.java
+++ Informatica/Programmazione/Java/Anagrafica/src/elenco/Main.java
@@ -1,8 +1,9 @@
 package elenco;
+
 public class Main {
-    public static void main(String[] args)  {
-        Persona P=new Persona("Alessandro","Sedda","17","1.70f");
+    public static void main(String[] args) {
+        Persona P = new Persona("Alessandro", "Sedda", 17, 1.70f);
         System.out.println(P.toString());
     }
-
 }
+
