e53f4bddc0f4aa1fb2b6ccc4d9dffc87fb483da9
ilMaestro <melonimirko@asproni-fermi.edu.it>
2025-11-11 12:55:41 +0100

    Commit automatico 11/11/2025 12:55:40,83

--- Informatica/Programmazione/Java/Catena autolavaggio/src/Catena/Main.java
+++ Informatica/Programmazione/Java/Catena autolavaggio/src/Catena/Main.java
@@ -1,5 +1,35 @@
+package Catena;
 public class Main {
     public static void main(String[] args) {
-        
+        // Creazione di due veicoli
+        Veicolo V1 = new Veicolo("AB123CD", "Fiat", "Panda", 1200, 2020, 5);
+        Veicolo V2 = new Veicolo("EF456GH", "Ford", "Focus", 1600, 2021, 5);
+
+        // Creazione di V3 (copia del riferimento di V1)
+        Veicolo V3 = V1; // stessa istanza
+
+        // Creazione di V4 (copia dei valori di V2)
+        Veicolo V4 = new Veicolo(V2); // nuovo oggetto con stessi dati
+
+        // Modifica di un attributo in V3 e V4
+        V3.setModello("Panda Cross");
+        V4.setModello("Focus Sport");
+
+        // Stampa dei veicoli per verificare gli effetti
+        System.out.println("V1: " + V1);
+        System.out.println("V2: " + V2);
+        System.out.println("V3: " + V3);
+        System.out.println("V4: " + V4);
+
+        // Confronto potenza
+        Veicolo piuPotente = V1.confronta(V2);
+        if (piuPotente == null)
+            System.out.println("Le due auto hanno la stessa cilindrata.");
+        else
+            System.out.println("L'auto più potente è: " + piuPotente.getModello());
+
+        // Verifica noleggio
+        int personeRichieste = 6;
+        System.out.println("Il veicolo V1 può trasportare " + personeRichieste + " persone? " + V1.noleggio(personeRichieste));
     }
 }
--- Informatica/Programmazione/Java/Catena autolavaggio/src/Catena/Veicolo.java
+++ Informatica/Programmazione/Java/Catena autolavaggio/src/Catena/Veicolo.java
@@ -1,3 +1,101 @@
+package Catena;
 public class Veicolo {
+    // Attributi
+    private String targa;
+    private String marca;
+    private String modello;
+    private int cilindrata;
+    private int annoAcquisto;
+    private int numPosti;
 
+    // Costruttore
+    public Veicolo(String targa, String marca, String modello, int cilindrata, int annoAcquisto, int numPosti) {
+        this.targa = targa;
+        this.marca = marca;
+        this.modello = modello;
+        this.cilindrata = cilindrata;
+        this.annoAcquisto = annoAcquisto;
+        this.numPosti = numPosti;
+    }
+
+    // Costruttore di copia
+    public Veicolo(Veicolo v) {
+        this.targa = v.targa;
+        this.marca = v.marca;
+        this.modello = v.modello;
+        this.cilindrata = v.cilindrata;
+        this.annoAcquisto = v.annoAcquisto;
+        this.numPosti = v.numPosti;
+    }
+
+    // Getter e Setter
+    public String getTarga() { 
+        return targa;
+     }
+    public void setTarga(String targa) { 
+        this.targa = targa; 
+    }
+
+    public String getMarca() {
+         return marca;
+         }
+    public void setMarca(String marca) {
+         this.marca = marca;
+         }
+
+    public String getModello() {
+         return modello; 
+        }
+    public void setModello(String modello) {
+         this.modello = modello; 
+        }
+
+    public int getCilindrata() {
+         return cilindrata; 
+        }
+    public void setCilindrata(int cilindrata) {
+         this.cilindrata = cilindrata;
+         }
+
+    public int getAnnoAcquisto() {
+         return annoAcquisto; 
+        }
+    public void setAnnoAcquisto(int annoAcquisto) {
+         this.annoAcquisto = annoAcquisto;
+         }
+
+    public int getNumPosti() {
+         return numPosti; 
+        }
+    public void setNumPosti(int numPosti) {
+         this.numPosti = numPosti; 
+        }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Veicolo{" +
+                "targa='" + targa + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modello='" + modello + '\'' +
+                ", cilindrata=" + cilindrata +
+                ", annoAcquisto=" + annoAcquisto +
+                ", numPosti=" + numPosti +
+                '}';
+    }
+
+    // Metodo confronta: restituisce il veicolo più potente
+    public Veicolo confronta(Veicolo altro) {
+        if (this.cilindrata > altro.cilindrata)
+            return this;
+        else if (this.cilindrata < altro.cilindrata)
+            return altro;
+        else
+            return null; // stessa potenza
+    }
+
+    // Metodo noleggio: verifica se i posti sono sufficienti
+    public boolean noleggio(int persone) {
+        return persone <= numPosti;
+    }
 }
