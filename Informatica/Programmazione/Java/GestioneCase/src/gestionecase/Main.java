3f017fd538ecdba8792a86c988974e8c475ebdb7
IlMaestro <melonimirko@asproni-fermi.edu.it>
2025-11-30 18:45:30 +0100

    Commit automatico 30/11/2025 18:45:30,72

--- Informatica/Programmazione/Java/GestioneCase/src/gestionecase/Main.java
+++ Informatica/Programmazione/Java/GestioneCase/src/gestionecase/Main.java
@@ -4,71 +4,47 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       Casa C1;
+       String indirizzo;
+       System.out.println("Scrivi l'indirizzo della casa: ");
+       indirizzo=s.nextLine();
+       int stanze;
+       System.out.println("Quante stanze ha la casa? ");
+       stanze=s.nextInt();
+       s.nextLine();
+       double a;
+       System.out.println("Indica la superficie della casa: ");
+       a=s.nextDouble();
+       s.nextLine();
+       double p;
+       System.out.println("Quanto costa la casa? ");
+       p=s.nextDouble();
+       s.nextLine();
+       boolean g;
+       System.out.println("Ha il giardino? (true/false)");
+       g=s.nextBoolean();
+       s.nextLine();
+       int anno;
+       System.out.println("In che anno è stata costruita? ");
+       anno=s.nextInt();
+       s.nextLine();
+       C1=new Casa(indirizzo, stanze, a, p, g, anno);
+               
+       Proprietario P1;
+       System.out.println("Inserisci i dati del proprietario\nNome: ");
+       String nome=s.nextLine();
+       System.out.println("Cognome: ");
+       String cognome=s.nextLine();
+       System.out.println("Codice Fiscale: ");
+       String codFiscale=s.nextLine();
+       P1=new Proprietario(nome, cognome, codFiscale, C1);
+       
+       System.out.println(P1.toString());
 
-        Scanner sc = new Scanner(System.in);
+       
+       
+   }
 
-        System.out.println("----- Inserisci dati Casa 1 -----");
-        Casa c1 = leggiCasa(sc);
-
-        System.out.println("\n----- Inserisci dati Casa 2 -----");
-        Casa c2 = leggiCasa(sc);
-
-        // Copia casa
-        Casa c3 = new Casa(c1);
-        c3.setIndirizzo("Via Napoli 3");
-        c3.setAnnoCostruzione(2010);
-
-        // Stampa case
-        System.out.println("\n----- Case -----");
-        System.out.println(c1);
-        System.out.println(c2);
-        System.out.println(c3);
-
-        // Confronti
-        System.out.println("\n----- Confronti -----");
-        System.out.println("Casa con superficie maggiore: " + c1.superficieMaggiore(c2));
-        System.out.println("Casa più costosa: " + c1.prezzoMaggiore(c2));
-
-        // Proprietari
-        Proprietario p1 = new Proprietario("Mario", "Rossi", "RSSMRA80A01H501U", c1);
-        Proprietario p2 = new Proprietario("Lucia", "Bianchi", "BNCLCU75A41F205K", c2);
-
-        Proprietario p3 = new Proprietario(p1);
-        p3.setNome("Giovanni");
-        p3.getCasa().setIndirizzo("Via Firenze 4");
-        p3.getCasa().setAnnoCostruzione(2015);
-
-        System.out.println("\n----- Proprietari -----");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-
-        sc.close();
-    }
-
-    // --- Legge una casa da tastiera ---
-    public static Casa leggiCasa(Scanner sc) {
-        System.out.print("Indirizzo: ");
-        String indirizzo = sc.nextLine();
-
-        System.out.print("Numero stanze: ");
-        int stanze = sc.nextInt();
-
-        System.out.print("Superficie (mq): ");
-        int superficie = sc.nextInt();
-
-        System.out.print("Prezzo: ");
-        double prezzo = sc.nextDouble();
-
-        System.out.print("Garage (true/false): ");
-        boolean garage = sc.nextBoolean();
-
-        System.out.print("Anno costruzione: ");
-        int anno = sc.nextInt();
-        sc.nextLine(); // pulisce il buffer
-
-        return new Casa(indirizzo, stanze, superficie, prezzo, garage, anno);
-    }
 }
-
