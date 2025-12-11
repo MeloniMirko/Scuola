aeecba8c5777474b2df30d9a20901e3833473df9
MeloniMirko <melonimirko@asproni-fermi.edu.it>
2025-12-11 18:48:53 +0100

    Commit automatico 11/12/2025 18:48:53,74

---
+++ TPSIT/Esercitazioni alla verifica/Verifica 12-12-2025/Registro Attivita Quotidiane/script.js
@@ -0,0 +1,8 @@
+function AggiungiAttivita(){
+    let nomeattivita = document.getElementById("NomeAttivita").value;
+    let durataattivita = Number(document.getElementById("DurataAttivita").value);
+
+    if (nomeattivita === ""){
+       alert("Inserisci un nome attivita") 
+    }
+}
