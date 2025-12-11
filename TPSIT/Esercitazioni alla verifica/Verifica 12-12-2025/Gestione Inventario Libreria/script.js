3a7b9d4f538e29eba01f4cdeb551c8dd89192ed6
MeloniMirko <melonimirko@asproni-fermi.edu.it>
2025-12-11 18:25:06 +0100

    Commit automatico 11/12/2025 18:25:05,96

--- TPSIT/Esercitazioni alla verifica/Verifica 12-12-2025/Gestione Inventario Libreria/script.js
+++ TPSIT/Esercitazioni alla verifica/Verifica 12-12-2025/Gestione Inventario Libreria/script.js
@@ -1,8 +1,8 @@
 function AggiungiLibro(){
     let titololibro = document.getElementById("Titolo").value;
-    let quantita = number(document.getElementById("Quantita").value);
+    let quantita = Number(document.getElementById("Quantita").value);
 
     if(titololibro && quantita == "" ){
-
+        document.getElementById("redalert").innerHTML = "non hai inserito nessun titolo o quantita riprova";
     }
 }
