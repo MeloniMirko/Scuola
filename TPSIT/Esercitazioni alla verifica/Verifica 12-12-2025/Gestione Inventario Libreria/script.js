4288b64a0d9a5f41729124467bd56854c1333b6d
MeloniMirko <melonimirko@asproni-fermi.edu.it>
2025-12-11 18:20:03 +0100

    Commit automatico 11/12/2025 18:20:03,40

---
+++ TPSIT/Esercitazioni alla verifica/Verifica 12-12-2025/Gestione Inventario Libreria/script.js
@@ -0,0 +1,8 @@
+function AggiungiLibro(){
+    let titololibro = document.getElementById("Titolo").value;
+    let quantita = number(document.getElementById("Quantita").value);
+
+    if(titololibro && quantita == "" ){
+
+    }
+}
