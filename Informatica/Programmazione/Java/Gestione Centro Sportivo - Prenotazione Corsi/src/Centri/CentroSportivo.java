86bc891adee4c97f03d9e497a618eaea3d59f9b2
ilMaestro <melonimirko@asproni-fermi.edu.it>
2026-03-10 13:27:47 +0100

    Commit automatico 10/03/2026 13:27:47,61

--- Informatica/Programmazione/Java/Gestione Centro Sportivo - Prenotazione Corsi/src/Centri/CentroSportivo.java
+++ Informatica/Programmazione/Java/Gestione Centro Sportivo - Prenotazione Corsi/src/Centri/CentroSportivo.java
@@ -1,21 +1,50 @@
 package Centri;
+
 public class CentroSportivo {
     private Corso[] corsi;
     private int numeroCorsi;
 
     public CentroSportivo() {
-        this.corsi = new Corso[10]; 
-        this.numeroCorsi = 0; 
+        this.corsi = new Corso[10];
+        this.numeroCorsi = 0;
     }
 
     public CentroSportivo(CentroSportivo altro) {
         this.corsi = new Corso[altro.corsi.length];
-    
         this.numeroCorsi = altro.numeroCorsi;
 
-        for(int i=0; i < this.numeroCorsi; i++){
-            this.corsi[i]=new Corso(altro.corsi[i]);
+        for(int i = 0; i < this.numeroCorsi; i++){
+            this.corsi[i] = new Corso(altro.corsi[i]);
         }
     }
+
+    public boolean aggiungiCorso(Corso c){
+        if(numeroCorsi < corsi.length){
+            corsi[numeroCorsi] = c;
+            numeroCorsi++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean rimuoviCorso(int posizione){
+        if(posizione >= 0 && posizione < numeroCorsi){
+    
+            for(int i = posizione; i < numeroCorsi - 1; i++){
+                corsi[i] = corsi[i + 1];
+            }
+    
+            corsi[numeroCorsi - 1] = null;
+            numeroCorsi--;
+    
+            return true;
+        }
+    
+        return false;
+    }
+
+    public boolean rimuoviPerNome(String nome){
         
+    }
+
 }
